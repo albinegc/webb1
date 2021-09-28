5b6df6f7cb095410039cb1ee0efaaa32cf9ba5e5
albinegc <albinegc@gmail.com>
2021-09-28 10:30:52 +0200

    j

--- db_connection.php
+++ db_connection.php
@@ -1,10 +1,12 @@
+
 <?php
+include ‘connection.php
 function OpenCon()
  {
  $dbhost = "localhost";
  $dbuser = "root";
- $dbpass = "1234";
- $db = "example";
+ $dbpass = "";
+ $db = "practice";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
  return $conn;
