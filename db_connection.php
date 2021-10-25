872dc1df8f8811cd65698dc95427314c8b82e214
albinegc <albinegc@gmail.com>
2021-10-25 13:05:29 +0200

    fixat

--- db_connection.php
+++ db_connection.php
@@ -1,18 +1,28 @@
 <?php
 function OpenCon()
- {
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "";
- $db = "practice";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+{
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$db = "test";
+$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
- return $conn;
- }
+return $conn;
+}
 
 function CloseCon($conn)
- {
- $conn -> close();
- }
+{
+$conn -> close();
+}
+
+$query = mysqli_query($conn, "SELECT * FROM tbl_articles");
+while($row = mysqli_fetch_assoc($query))
+{
+$title = $row[´title´];
+$content = $row [´content´];
+}
+
+
+
 
 ?>
